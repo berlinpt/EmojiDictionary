2508fcdd97386624c3b740c83a578c480562fcc3
Berlin <berlinpt@gmail.com>
2024-03-18 15:42:19 -0400

    Initial Commit

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Berlin Thomas on 2024-03-18.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
+
+
