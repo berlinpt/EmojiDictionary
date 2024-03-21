7d189ab70ef135e853dbf4b3fb4025b37b1e9476
Berlin <berlinpt@gmail.com>
2024-03-20 21:59:58 -0400

    Saving Data implementation

--- EmojiDictionary/Emoji.swift
+++ EmojiDictionary/Emoji.swift
@@ -7,11 +7,46 @@
 
 import Foundation
 
-struct Emoji {
+struct Emoji: Codable {
     var symbol: String
     var name: String
     var description: String
     var usage: String
+    
+    static var documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let archiveURL = documentsPath.appendingPathComponent("emojis").appendingPathExtension("plist")
+    
+   static func saveToFile(emojis: [Emoji]) {
+        let propertyListEncoder = PropertyListEncoder()
+        let codedEmojis = try? propertyListEncoder.encode(emojis)
+        try? codedEmojis?.write(to: Emoji.archiveURL, options: .noFileProtection)
+    }
+    
+    static func loadFromFile() -> [Emoji]? {
+        guard let codedEmojis = try? Data(contentsOf: Emoji.archiveURL) else { return nil }
+        let propertyListDecoder = PropertyListDecoder()
+        return try? propertyListDecoder.decode([Emoji].self, from: codedEmojis)
+    }
+
+    static func loadSampleEmojis() -> [Emoji] {
+        let emojis: [Emoji] = [
+            Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"),
+            Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
+            Emoji(symbol: "😍", name: "Heart Eyes", description: "A smiley face with hearts for eyes.", usage: "love of something; attractive"),
+            Emoji(symbol: "🧑‍💻", name: "Developer", description: "A person working on a MacBook (probably using Xcode to write iOS apps in Swift).", usage: "apps, software, programming"),
+            Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "Something slow"),
+            Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"),
+            Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"),
+            Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
+            Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
+            Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "homework, studying"),
+            Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.", usage: "extreme sadness"),
+            Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
+            Emoji(symbol: "🏁", name: "Checkered Flag", description: "A black-and-white checkered flag.", usage: "completion")
+        ]
+        
+        return emojis
+    }
 }
 
 
--- EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -9,24 +9,17 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
     
-    var emojis: [Emoji] = [
-        Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"),
-        Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
-        Emoji(symbol: "😍", name: "Heart Eyes", description: "A smiley face with hearts for eyes.", usage: "love of something; attractive"),
-        Emoji(symbol: "🧑‍💻", name: "Developer", description: "A person working on a MacBook (probably using Xcode to write iOS apps in Swift).", usage: "apps, software, programming"),
-        Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "Something slow"),
-        Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"),
-        Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"),
-        Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
-        Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
-        Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.", usage: "homework, studying"),
-        Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.", usage: "extreme sadness"),
-        Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
-        Emoji(symbol: "🏁", name: "Checkered Flag", description: "A black-and-white checkered flag.", usage: "completion")
-    ]
+    var emojis: [Emoji] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let savedEmojis = Emoji.loadFromFile() {
+            emojis = savedEmojis
+        }
+        else {
+            emojis = Emoji.loadSampleEmojis()
+        }
         
         tableView.cellLayoutMarginsFollowReadableWidth = true
         
@@ -119,6 +112,8 @@ class EmojiTableViewController: UITableViewController {
             emojis.append(emoji)
             tableView.insertRows(at: [newIndexPath], with: .automatic)
         }
+        
+        Emoji.saveToFile(emojis: emojis)
     }
     
 
